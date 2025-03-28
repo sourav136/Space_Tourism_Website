32190d5f3e4de167843c09f512bf9c6801c9e7ad
Sourav <souravbarua789bd@gmail.com>
2025-03-28 11:38:10 +0600

    added nimation to home

--- src/assets/components/Home/Home.jsx
+++ src/assets/components/Home/Home.jsx
@@ -8,12 +8,12 @@ const Home = () => {
             <div className="home">
                 <div className="container home-container">
                     <div className="row">
-                        <div className="col-12 col-lg-6">
+                        <div className="col-12 col-lg-6 home-text">
                             <h4 className='home-h4'>SO, YOU WANT TO TRAVEL TO</h4>
                             <h1 className='home-h1'>SPACE</h1>
                             <p className='home-p'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
                         </div>
-                        <div className="col-12 col-lg-6 d-flex align-items-center justify-content-center">
+                        <div className="col-12 col-lg-6 d-flex align-items-center justify-content-center home-button">
                             <Link to="/destination" className='explore-button' tabIndex={0}>Explore</Link>
                         </div>
                     </div>
