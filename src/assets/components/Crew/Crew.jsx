1063d28dcf3ac5ef867473054751aec720d5aa86
Sourav <souravbarua789bd@gmail.com>
2025-03-30 09:07:50 +0600

    added alt text to crew slider images

--- src/assets/components/Crew/Crew.jsx
+++ src/assets/components/Crew/Crew.jsx
@@ -34,7 +34,7 @@ const Crew = () => {
                                     <p className='crew-p'>Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.</p>
                                 </div>
                                 <div className="col-12 col-lg-6 crew-image-container">
-                                    <img className='crew-image' src={Crew1} alt="" />
+                                    <img className='crew-image' src={Crew1} alt="Commander Douglas Hurley" />
                                 </div>
                             </div>
                         </SwiperSlide>
@@ -47,7 +47,7 @@ const Crew = () => {
                                     <p className='crew-p'>Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.</p>
                                 </div>
                                 <div className="col-12 col-lg-6 crew-image-container">
-                                    <img className='crew-image' src={Crew2} alt="" />
+                                    <img className='crew-image' src={Crew2} alt="Mission Specialist MARK SHUTTLEWORTH" />
                                 </div>
                             </div>
                         </SwiperSlide>
@@ -60,7 +60,7 @@ const Crew = () => {
                                     <p className='crew-p'>Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer. </p>
                                 </div>
                                 <div className="col-12 col-lg-6 crew-image-container">
-                                    <img className='crew-image' src={Crew3} alt="" />
+                                    <img className='crew-image' src={Crew3} alt="PILOT Victor Glover" />
                                 </div>
                             </div>
                         </SwiperSlide>
@@ -73,7 +73,7 @@ const Crew = () => {
                                     <p className='crew-p'>Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.</p>
                                 </div>
                                 <div className="col-12 col-lg-6 crew-image-container">
-                                    <img className='crew-image' src={Crew4} alt="" />
+                                    <img className='crew-image' src={Crew4} alt="Flight Engineer Anousheh Ansari" />
                                 </div>
                             </div>
                         </SwiperSlide>
